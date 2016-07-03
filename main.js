a7b57d79e4803849c9a7e13bc9798b48a510c8cb
MegaMech <MegaMech@users.noreply.github.com>
2016-07-02 22:52:07 -0700

    file upload

--- main.js
+++ main.js
@@ -2,17 +2,7 @@
 
 
 function menu(selection) {
-
-var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-  console.log(this.responseXML.title);
-  a = this.responseXML;
-  document.getElementsByTagName("content").innerHTML = this.responseXML.getElementsByTagName("p")[0].innerHTML;
-}
-xhr.open("GET", selection+".html");
-xhr.responseType = "document";
-xhr.send();
-/*	var menu = document.getElementsByClassName("buttonmobilewrap")[0];
+	var menu = document.getElementsByClassName("buttonmobilewrap")[0];
 	if (selection == "activate") {
 		if (menu.clientHeight == "400") {
 			menu.style.height = "0px";
@@ -22,22 +12,14 @@ xhr.send();
 		}
 		return true;
 	}
-	
-var xmlhttp;
-  if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    // code for older browsers
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      document.getElementsByTagName("content").innerHTML = xmlhttp.responseText;
-    }
-  };
-  xmlhttp.open("GET", selection+".html", true);
-  xmlhttp.responseType = "document";
-  xmlhttp.send();*/
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+	a = this.responseXML;
+	document.getElementsByTagName("content")[0].innerHTML = this.responseXML.getElementsByTagName("p")[0].innerHTML;
+}
+xhr.open("GET", selection+".html");
+xhr.responseType = "document";
+xhr.send();
 }
 function menuPos() {
 	if (window.innerWidth <= 768) {
