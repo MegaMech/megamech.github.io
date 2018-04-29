8229905b09ceebb44476c8f244c55c8e8b178f66
MegaMech <MegaMech@users.noreply.github.com>
2018-04-29 15:31:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ function menu(selection) {
 	if (this.responseXML.getElementsByTagName("body")[0].childNodes[2] != null) {
 		document.getElementsByTagName("content")[1].innerHTML = this.responseXML.getElementsByTagName("body")[0].childNodes[2].innerHTML;
 	}
+	else {document.getElementsByTagName("content")[1].innerHTML = "";}
 		console.log(this.responseXML.getElementsByTagName("body")[0].innerHTML);
 }
 window.location.hash = selection;
