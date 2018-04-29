3e833b7761805cde1fd6d81cd8f356e4e46d2dee
MegaMech <MegaMech@users.noreply.github.com>
2018-04-28 23:16:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ function menu(selection) {
 	xhr.onload = function() {
 	a = this.responseXML;
 	document.getElementsByTagName("content")[0].innerHTML = this.responseXML.getElementsByTagName("body")[0].childNodes[0].innerHTML;
-	document.getElementsByTagName("content")[1].innerHTML = this.responseXML.getElementsByTagName("body")[0].childNodes[1].innerHTML;
+	document.getElementsByTagName("content")[1].innerHTML = this.responseXML.getElementsByTagName("body")[0].childNodes[2].innerHTML;
 	console.log(this.responseXML.getElementsByTagName("body")[0].innerHTML);
-		console.log(this.responseXML.getElementsByTagName("body")[0].childNodes[0].innerHTML);
-		console.log(this.responseXML.getElementsByTagName("body")[0].childNodes[1].innerHTML);
+		console.log(this.responseXML.getElementsByTagName("body")[0].childNodes[3].innerHTML);
+		console.log(this.responseXML.getElementsByTagName("body")[0].childNodes[4].innerHTML);
 }
 window.location.hash = selection;
 xhr.open("GET", selection+".html");
