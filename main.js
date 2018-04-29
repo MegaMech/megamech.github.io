76fe10663ec364d4a397965afb52ac99ddb8f4b5
MegaMech <MegaMech@users.noreply.github.com>
2018-04-28 22:53:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function menu(selection) {
 	xhr.onload = function() {
 	a = this.responseXML;
 	document.getElementsByTagName("content")[0].innerHTML = this.responseXML.getElementsByTagName("body")[0].innerHTML;
-	document.getElementsByTagName("content")[1].innerHTML = this.responseXML.getElementsByClassName("loadedcontent_b")[0].innerHTML;
+	document.getElementsByTagName("content")[1].innerHTML = this.responseXML.getElementsByTagName("body")[0].childNodes[5].innerHTML;
 	console.log(this.responseXML.getElementsByTagName("body")[0].innerHTML);
 }
 window.location.hash = selection;
