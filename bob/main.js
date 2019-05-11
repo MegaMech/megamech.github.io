96020238f5070f300e055f1224dc69cfbeac5571
MegaMech <MegaMech@users.noreply.github.com>
2019-05-10 18:43:02 -0600

    Add files via upload

--- bob/main.js
+++ bob/main.js
@@ -7,9 +7,11 @@ window.onload = function()
     //var x = window.matchMedia("(max-width: 800px)")
     spaceSetter(x);
     x.addListener(spaceSetter);
-    var menus = document.getElementsByClassName("cellphoneMenuPanel")[0];
 
-menus.style.height = "0px";
+    var menus = document.getElementsByClassName("cellphoneMenuPanel")[0];
+    menus.style.height = "0px";
+
+    footerLink();
 }
 
 function menu() {
@@ -37,7 +39,6 @@ function preview(img)
 }
 function spaceSetter() {
     var imgs = document.getElementsByClassName("imgSpacer");
-    console.log("running");
   if (x.matches) { // If media query matches
 
     for (i=0; i<imgs.length; i++)
@@ -52,4 +53,29 @@ function spaceSetter() {
       document.getElementsByClassName("imgSpacer")[i].style.height = "0px";
     }
   }
+}
+function footerLink()
+{
+    var o = "e";
+    var idf = "b";
+    var c = "o";
+    var efjei = "n";
+    var idje = "nce"
+    var ei = "sk";
+    var ehdidoe = "@";
+    var a = "ma";
+    var ioda = "as";
+    var l = "nt";
+    var dats = ".";
+    var mn = "t";
+    var ied = "p";
+    var d = "i";
+    var ijd = "bu";
+    var z = "lt";
+    var x = "r";
+
+    document.getElementsByClassName("footerLink")[0].href = a+d+z+c+":"+idf+c+idf+ijd+ei+ioda+ehdidoe+d+idje+l+x+o+dats+efjei+o+mn;
+    document.getElementsByClassName("footerLink")[1].href = a+d+z+c+":"+idf+c+idf+ijd+ei+ioda+ehdidoe+d+idje+l+x+o+dats+efjei+o+mn;
+    document.getElementsByClassName("footerLink")[0].innerHTML = idf+c+idf+ijd+ei+ioda+ehdidoe+d+idje+l+x+o+dats+efjei+o+mn;
+    document.getElementsByClassName("footerLink")[1].innerHTML = idf+c+idf+ijd+ei+ioda+ehdidoe+d+idje+l+x+o+dats+efjei+o+mn;
 }
