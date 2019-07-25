e9e41e7c089b8346837210f1b72c6fa74d038632
MegaMech <MegaMech@users.noreply.github.com>
2019-07-25 01:56:45 -0600

    Add files via upload

---
+++ search/itemDB.js
@@ -0,0 +1,114 @@
+const items = [
+{
+    id: "12",
+    item: "Pickaxe",
+    mat:
+    [
+        "100",
+        "101",
+        "102"
+    ],
+    stats:
+    [
+        {
+            "Damage":"1",
+            "Mining":"5",
+            "Text":"A blunt Pickaxe"
+        }
+    ]
+},
+{
+    id: "13",
+    item: "Sword",
+    mat:
+    [
+        "103",
+        "104",
+        "105"
+    ],
+    stats:
+    [
+        {
+            "Damage":"5",
+            "Text":"A very sharp sword"
+        }
+    ]
+},
+{
+    id: "14",
+    item: "Hammer",
+    mat:
+    [
+        "103",
+        "101",
+        "105"
+    ],
+    stats:
+    [
+        {
+            "Damage":"3",
+            "Mining":"1",
+            "Text":"Oooh, heavy weapon!"
+        }
+    ]
+},
+{
+    id: "15",
+    item: "Spanner",
+    mat:
+    [
+        "103"
+    ],
+    stats:
+    [
+        {
+            "Damage":"4",
+            "Text":"A British word for &apos;wrench&apos;"
+        }
+    ]
+},
+{
+    id: "16",
+    item: "Club",
+    mat:
+    [
+        "100",
+        "100",
+        "101"
+    ],
+    stats:
+    [
+        {
+            "Damage":"5",
+            "Text":"It&apos;s clubbing time"
+        }
+    ]
+}
+]
+
+const resources = [
+{
+    id: "100",
+    item: "Rock"
+},
+{
+    id: "101",
+    item: "Stick"
+},
+{
+    id: "102",
+    item: "String"
+},
+{
+    id: "103",
+    item: "Steel"
+},
+{
+    id: "104",
+    item: "Handle"
+},
+{
+    id: "105",
+    item: "Rope"
+}
+]
--- search/main.js
+++ search/main.js
@@ -33,10 +33,11 @@ function searchDB(term)
         //if (term.value == "") {out.innerHTML = "Search is empty"; return;}
 
 
-        if (term.value == "") {out.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>Search is empty</span>"; return;}
-        if (result[0] == undefined) {out.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>No materials found</span>"; return;}
+        if (term.value === "") {out.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>Search is empty</span>"; return;}
+        if (result[0] === undefined) {out.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>No materials found</span>"; return;}
 
         out.innerHTML = "";
+        var i;
         for (i=0; i < result.length; i++)
         {
             out.innerHTML += "<button class='searchButtons' onclick='select(this, "+result[i].id+");'>"+result[i].item+" | "+result[i].id+"</button>"+"<br>";
@@ -46,7 +47,7 @@ function searchDB(term)
 //out.innerHTML += (result[i].id+" | "+result[i].item+" | "+result[i].mat+"<br>");
 function select(selection, id)
 {
-    document.getElementById("selection").innerHTML += "<button class='searchButtons elements' data='"+id+"' onclick='this.remove(); mainSearch();' style='display: inline-block'>"+selection.innerHTML+"</button>"
+    document.getElementById("selection").innerHTML += "<button class='searchButtons elements' data='"+id+"' onclick='this.remove(); mainSearch();' style='display: inline-block'>"+selection.innerHTML+"</button>";
     textFit(document.getElementsByClassName("elements"));
     mainSearch();
 }
@@ -54,9 +55,10 @@ function mainSearch()
 {
     var elements = document.getElementsByClassName("elements");
     var out2 = document.getElementById("output2");
-    if (elements[0] == undefined) {out2.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>Materials required to search</span>"; return;}
-    if (elements[30] != undefined) {return;}
+    if (elements[0] === undefined) {out2.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>Materials required to search</span>"; return;}
+    if (elements[30] !== undefined) {return;}
     var data = [];
+    var i;
     for(i=0; i < elements.length; i++) {
         data.push(document.getElementsByClassName("elements")[i].getAttribute("data"));
     }
@@ -65,15 +67,28 @@ function mainSearch()
     console.log(joinData);
     var result2 = fuse2.search(joinData);
     console.log(result2);
-    if (result2[0] == undefined) {out2.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>No items found</span>"; return;}
+    if (result2[0] === undefined) {out2.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>No items found</span>"; return;}
 
     out2.innerHTML = "";
-    for (i=0; i < result2.length; i++)
+    var j;
+    for (j=0; j < result2.length; j++)
     {
-        out2.innerHTML += "<button class='searchButtons');' onclick='itemStats(this, "+result2[i].id+")'>"+result2[i].item+" | "+result2[i].id+" | "+result2[i].mat+"</button>"+"<br>";
+        out2.innerHTML += "<button class='searchButtons');' onclick='itemStats(this, "+JSON.stringify(result2[j])+");'>"+result2[j].item+" | "+result2[j].id+" | "+result2[j].mat+"</button>"+"<br>";
     }
 }
-function itemStats(selection, id)
+var isOpen = false;
+function itemStats(selection, data)
 {
+    var stats = document.getElementById("itemStats");
 
+    stats.style.left = selection.getBoundingClientRect().left+"px";
+    stats.style.top = selection.getBoundingClientRect().top+"px";
+    stats.style.display = "block";
+    document.getElementById("cover").style.display = "block";
+    stats.innerHTML = data.item+" | ID: "+data.id+"<br>"+"<span style='font-style: italic'>"+data.stats[0].Text+"</span>"+"<br><br>"+"Damage: "+data.stats[0].Damage+"<br>"+"Mining: "+data.stats[0].Mining;
+}
+function closeItemStats()
+{
+    document.getElementById("itemStats").style.display = "none";
+    document.getElementById("cover").style.display = "none";
 }
