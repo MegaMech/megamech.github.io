b43615945ce5fd41c2cfc113a381b46290edf592
MegaMech <MegaMech@users.noreply.github.com>
2019-07-24 18:17:56 -0600

    Add files via upload

--- search/main.js
+++ search/main.js
@@ -1,4 +1,4 @@
-var options = {
+const options = {
     shouldSort: true,
     threshold: 0.6,
     location: 0,
@@ -10,9 +10,10 @@ var options = {
         "item", "id"
     ]
 };
-var options2 = {
+const options2 = {
     shouldSort: true,
     threshold: 0.6,
+    findAllMatches: true,
     location: 0,
     distance: 100,
     maxPatternLength: 32,
@@ -33,8 +34,8 @@ function searchDB(term)
         //if (term.value == "") {out.innerHTML = "Search is empty"; return;}
 
 
-        if (term.value == "") {out.innerHTML = "Search is empty"; return;}
-        if (result[0] == undefined) {out.innerHTML = "No results found"; return;}
+        if (term.value == "") {out.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>Search is empty</span>"; return;}
+        if (result[0] == undefined) {out.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>No materials found</span>"; return;}
 
         out.innerHTML = "";
         for (i=0; i < result.length; i++)
@@ -46,26 +47,23 @@ function searchDB(term)
 //out.innerHTML += (result[i].id+" | "+result[i].item+" | "+result[i].mat+"<br>");
 function select(selection, id)
 {
-    console.log(selection);
     document.getElementById("selection").innerHTML += "<button class='searchButtons elements' data='"+id+"' onclick='this.remove(); mainSearch();' style='display: inline-block'>"+selection.innerHTML+"</button>"
+    textFit(document.getElementsByClassName("elements"));
     mainSearch();
 }
 function mainSearch()
 {
     var elements = document.getElementsByClassName("elements");
     var out2 = document.getElementById("output2");
-    if (elements[0] == undefined) {out2.innerHTML = "Elements needed to search"; return;}
+    if (elements[0] == undefined) {out2.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>Materials required to search</span>"; return;}
     if (elements[30] != undefined) {return;}
     var data = [];
     for(i=0; i < elements.length; i++) {
         data.push(document.getElementsByClassName("elements")[i].getAttribute("data"));
     }
-
-
-    console.log(data.join())
+    console.log(data.join());
     var result2 = fuse2.search(data.join());
-    console.log(result2);
-    if (result2[0] == undefined) {out2.innerHTML = "No results found"; return;}
+    if (result2[0] == undefined) {out2.innerHTML = "<span style='width: 100%; margin-top: 20px; display: block; text-align: center;'>No items found</span>"; return;}
 
     out2.innerHTML = "";
     for (i=0; i < result2.length; i++)
