18a3e96b2432e9e38796473a85c4cb4d2634d544
MegaMech <MegaMech@users.noreply.github.com>
2019-08-18 20:13:04 -0700

    Add files via upload

--- bob/comics/comics.js
+++ bob/comics/comics.js
@@ -3,10 +3,10 @@ comics = {
     1:{Title:"Batman", Date:"August 05, 2019", FileName:"batman.jpeg"},
     2:{Title:"Biggest Fan", Date:"August 06, 2019", FileName:"fan.jpg"},
     3:{Title:"Chevy sucks", Date:"August 07, 2019", FileName:"foghorn.jpg"},
-    5:{Title:"Cymbol Tryouts", Date:"August 08, 2019", FileName:"cymbols.jpg"},
-    6:{Title:"Cheese is a Loaf of Milk", Date:"August 09, 2019", FileName:"cheese.jpg"},
-    7:{Title:"Raisins are Grape Jerky", Date:"August 10, 2019", FileName:"raisins.jpg"},
-    8:{Title:"Furrasic Fark", Date:"August 11, 2019", FileName:"jurrasicpark.png"},
-    9:{Title:"Dog Man", Date:"August 12, 2019", FileName:"dogman.jpg"}
+    4:{Title:"Cymbol Tryouts", Date:"August 08, 2019", FileName:"cymbols.jpg"},
+    5:{Title:"Cheese is a Loaf of Milk", Date:"August 09, 2019", FileName:"cheese.jpg"},
+    6:{Title:"Raisins are Grape Jerky", Date:"August 10, 2019", FileName:"raisins.jpg"},
+    7:{Title:"Furrasic Fark", Date:"August 11, 2019", FileName:"jurrasicpark.png"},
+    8:{Title:"Dog Man", Date:"August 12, 2019", FileName:"dogman.jpg"}
 
 }
