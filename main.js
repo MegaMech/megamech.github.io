3f054d1a975a9256bfba1a47d32c747882dd92d3
MegaMech <MegaMech@users.noreply.github.com>
2020-08-31 13:41:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -53,6 +53,16 @@ function menu(selection) {
 function arrow() {
 	window.scrollTo({left: 0, top: window.innerHeight, behavior: 'smooth'});
 }
+function textCounter(field,field2,maxlimit) {
+	var countfield = document.getElementById(field2);
+ 	if (field.value.length > maxlimit) {
+ 		field.value = field.value.substring(0, maxlimit);
+ 		return false;
+ 	}
+	else {
+  		countfield.value = maxlimit - field.value.length;
+ 	}
+} 
 
 function nonsenseLink()
 {
