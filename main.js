4f68ad8ec268b0686c3290628f11daff330cdb04
MegaMech <MegaMech@users.noreply.github.com>
2020-09-01 17:55:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,9 @@ function menu(selection) {
 	}
 	else {document.getElementsByTagName("content")[1].innerHTML = "";}
 	window.scrollTo(0, 0);
+	
+		if (arrow) {window.scrollTo({left: 0, top: window.innerHeight, behavior: 'smooth'}); arrow = false;}
+	/*
 	if (arrow) {
 		if (window.innerWidth <= 835) {
 			window.scrollTo({left: 0, top: (window.innerHeight / 2), behavior: 'smooth'}); arrow = false;
@@ -52,6 +55,7 @@ function menu(selection) {
 		}
 		
 	}
+	*/
 	if (selection == "media") {window.scrollTo(0, 0);}
 	window.location.hash = selection;
 	xhr.open("GET", selection+".html");
@@ -60,12 +64,13 @@ function menu(selection) {
 }
 
 function arrow() {
+	/*
 	if (window.innerWidth <= 835) {
 		window.scrollTo({left: 0, top: (window.innerHeight / 2), behavior: 'smooth'}); arrow = false;
 	}
 	else {
 		window.scrollTo({left: 0, top: window.innerHeight, behavior: 'smooth'}); arrow = false;
-	}
+	}*/
 }
 
 function textCounter(e, counter) {
