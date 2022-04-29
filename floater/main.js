c6251bc7128cf00502d81647eea51d5315805c5a
MegaMech <MegaMech@users.noreply.github.com>
2022-04-29 16:05:26 -0600

    Add files via upload

--- floater/main.js
+++ floater/main.js
@@ -8,25 +8,25 @@ window.onload = function()
     particlesJS.load('particles-js', 'particles.json', function() {
       console.log('callback - particles.js config loaded');
     });
-     setTimeout(function(){
-         document.getElementById("loadspin").style.opacity = 0;
-         document.getElementById("logoImg").style.width = "450px";
-         document.getElementById("logoImg").style.marginTop = "2%";
-         setTimeout(function(){
-             document.getElementsByTagName("body")[0].style.overflow = "auto";
-             document.getElementsByTagName("body")[0].style.position = "relative";
-             document.getElementsByTagName("nav")[0].style.zIndex = "15";
-             document.getElementsByTagName("nav")[0].style.opacity = "1";
-             document.getElementById("contentDisplay").style.zIndex = "1";
-             document.getElementById("contentDisplay").style.opacity = "1";
-             document.getElementsByTagName("footer")[0].style.zIndex = "1";
-             document.getElementsByTagName("footer")[0].style.opacity = "1";
-             document.getElementById("loadspin").remove();
-             position = Object.keys(comics).length - 1;
-             comic(0);
-         }, 1000);
+    //  setTimeout(function(){
+    //      document.getElementById("loadspin").style.opacity = 0;
+    //      document.getElementById("logoImg").style.width = "450px";
+    //      document.getElementById("logoImg").style.marginTop = "2%";
+    //      setTimeout(function(){
+              document.getElementsByTagName("body")[0].style.overflow = "auto";
+              document.getElementsByTagName("body")[0].style.position = "relative";
+              document.getElementsByTagName("nav")[0].style.zIndex = "15";
+              document.getElementsByTagName("nav")[0].style.opacity = "0";
+              document.getElementById("contentDisplay").style.zIndex = "1";
+              document.getElementById("contentDisplay").style.opacity = "1";
+              document.getElementsByTagName("footer")[0].style.zIndex = "1";
+              document.getElementsByTagName("footer")[0].style.opacity = "1";
+    //          document.getElementById("loadspin").remove();
+    //          position = Object.keys(comics).length - 1;
+    //          comic(0);
+    //      }, 1000);
 
-     }, 1000);
+    //  }, 1000);
 }
 
 function menu()
